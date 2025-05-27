8c72b569db893b08aaf768f695a27aaa33876812
lidary-byte <lidaryl@163.com>
2025-05-27 14:08:25 +0800

    feat:  路由优化

--- lib/route/app_router.dart
+++ lib/route/app_router.dart
@@ -1,14 +1,17 @@
 import 'package:flutter_rss_reader/pages/built_in_feed/built_in_feed_page.dart';
 import 'package:flutter_rss_reader/pages/feed/edit_feed/edit_feed_page.dart';
 import 'package:flutter_rss_reader/pages/feed/feed_page.dart';
+import 'package:flutter_rss_reader/pages/home/home_binding.dart';
 import 'package:flutter_rss_reader/pages/home/home_page.dart';
 import 'package:flutter_rss_reader/pages/read/read_page.dart';
 import 'package:flutter_rss_reader/pages/read/read_setting_page/read_setting_page.dart';
 import 'package:flutter_rss_reader/pages/setting/block_setting_page/block_setting_page.dart';
 import 'package:flutter_rss_reader/pages/setting/font_setting_page/font_setting_page.dart';
 import 'package:flutter_rss_reader/pages/setting/language_setting_page/language_setting_page.dart';
-import 'package:flutter_rss_reader/pages/setting/theme_setting_page/theme_setting_page.dart';
+import 'package:flutter_rss_reader/pages/setting/setting_binding.dart';
+import 'package:flutter_rss_reader/pages/setting/theme_setting_page.dart';
 import 'package:flutter_rss_reader/pages/web_view/web_view_page.dart';
+import 'package:get/get.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 
 abstract class AppRouter {
@@ -28,46 +31,60 @@ abstract class AppRouter {
   static const String builtInFeedPageRouter = '/built_in_feed_page';
 
   static List<GetPage> routerPages = [
-    GetPage(name: homePageRouter, page: () => HomePage()),
     GetPage(
-        name: feedPageRouter,
-        page: () => FeedPage(),
-        transition: Transition.rightToLeft),
+      name: homePageRouter,
+      page: () => HomePage(),
+      bindings: [HomeBinding(), SettingBinding()],
+    ),
     GetPage(
-        name: editFeedPageRouter,
-        page: () => EditFeedPage(),
-        transition: Transition.rightToLeft),
+      name: feedPageRouter,
+      page: () => FeedPage(),
+      transition: Transition.rightToLeft,
+    ),
     GetPage(
-        name: languageSettingPageRouter,
-        page: () => LanguageSettingPage(),
-        transition: Transition.rightToLeft),
+      name: editFeedPageRouter,
+      page: () => EditFeedPage(),
+      transition: Transition.rightToLeft,
+    ),
     GetPage(
-        name: themeSettingPageRouter,
-        page: () => ThemeSettingPage(),
-        transition: Transition.rightToLeft),
+      name: languageSettingPageRouter,
+      page: () => LanguageSettingPage(),
+      transition: Transition.rightToLeft,
+    ),
     GetPage(
-        name: fontSettingPage,
-        page: () => FontSettingPage(),
-        transition: Transition.rightToLeft),
+      name: themeSettingPageRouter,
+      page: () => ThemeSettingPage(),
+      transition: Transition.rightToLeft,
+    ),
     GetPage(
-        name: readSettingPage,
-        page: () => ReadSettingPage(),
-        transition: Transition.rightToLeft),
+      name: fontSettingPage,
+      page: () => FontSettingPage(),
+      transition: Transition.rightToLeft,
+    ),
     GetPage(
-        name: blockSettingPage,
-        page: () => BlockSettingPage(),
-        transition: Transition.rightToLeft),
+      name: readSettingPage,
+      page: () => ReadSettingPage(),
+      transition: Transition.rightToLeft,
+    ),
     GetPage(
-        name: readPageRouter,
-        page: () => ReadPage(),
-        transition: Transition.rightToLeft),
+      name: blockSettingPage,
+      page: () => BlockSettingPage(),
+      transition: Transition.rightToLeft,
+    ),
     GetPage(
-        name: webViewPageRouter,
-        page: () => WebViewPage(),
-        transition: Transition.rightToLeft),
+      name: readPageRouter,
+      page: () => ReadPage(),
+      transition: Transition.rightToLeft,
+    ),
     GetPage(
-        name: builtInFeedPageRouter,
-        page: () => BuiltInFeedPage(),
-        transition: Transition.rightToLeft),
+      name: webViewPageRouter,
+      page: () => WebViewPage(),
+      transition: Transition.rightToLeft,
+    ),
+    GetPage(
+      name: builtInFeedPageRouter,
+      page: () => BuiltInFeedPage(),
+      transition: Transition.rightToLeft,
+    ),
   ];
 }
